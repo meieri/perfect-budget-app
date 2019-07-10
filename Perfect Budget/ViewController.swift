d1f3a22b244690da7675221dad732c3ce7260b7f
Isaak Meier <imeier@rightpoint.com>
2019-07-10 09:33:09 -0400

    Initial Commit

---
+++ Perfect Budget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Perfect Budget
+//
+//  Created by Isaak Meier on 7/10/19.
+//  Copyright © 2019 Isaak Meier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
