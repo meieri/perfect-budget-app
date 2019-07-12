dfac6aa6e42747fe5602eebc515de0a6577b8dc2
Isaak Meier <imeier@rightpoint.com>
2019-07-12 12:52:04 -0400

    made the progess bar real nice

--- Perfect Budget/ProgessBarView.swift
+++ Perfect Budget/ProgessBarView.swift
@@ -34,13 +34,48 @@ private extension ProgessBarView {
 
         // Style -- handled (partially) by the override of draw function
         progessBar.setProgress( Float(currentSpending) / Float(maxSpending), animated: true)
-        progessBar.layer.cornerRadius = 8.0
-        progessBar.clipsToBounds = true
+        configureTrack()
 
         // Layout
         // Sets them to be the same size, the size of this is handled in NameAndProgessView
         //self.
         progessBar.edgeAnchors == self.edgeAnchors
 
+    }
+
+    func configureTrack() {
+        progessBar.layer.cornerRadius = 16.0
+        progessBar.clipsToBounds = true
+
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 140, height: 140))
+        let img: UIImage = renderer.image { (context) in
+            // this is the image border
+            UIColor(displayP3Red: 133/255, green: 187/255, blue: 101/255, alpha: 1).setStroke()
+            // I don't actually know what this does
+            context.stroke(renderer.format.bounds)
+            // The color of the image itself
+            UIColor(displayP3Red: 133/255, green: 187/255, blue: 101/255, alpha: 1).setFill()
+            // fill her up
+            context.fill(CGRect(x: 1, y: 1, width: 140, height: 140))
+
+            UIColor.black.setStroke()
+            UIColor.black.setFill()
+            context.cgContext.fillEllipse(in: CGRect(x: 110, y: 55, width: 30, height: 30))
+        }
+        let nineSlice = img.roundedImage.resizableImage(withCapInsets: UIEdgeInsets.init(top: 15, left: 15, bottom: 15, right: 15))
+        progessBar.progressImage = nineSlice
+    }
+}
+
+extension UIImage{
+    var roundedImage: UIImage {
+        let rect = CGRect(origin:CGPoint(x: 0, y: 0), size: self.size)
+        UIGraphicsBeginImageContextWithOptions(self.size, false, 1)
+        UIBezierPath(
+            roundedRect: rect,
+            cornerRadius: 16.0
+            ).addClip()
+        self.draw(in: rect)
+        return UIGraphicsGetImageFromCurrentImageContext()!
     }
 }
