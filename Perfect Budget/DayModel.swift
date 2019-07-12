28c8297fd1489b2783e2c9b3c0adffc49929eb60
Isaak Meier <imeier@rightpoint.com>
2019-07-12 14:11:56 -0400

    more progess bar updates, made the height dynamic

---
+++ Perfect Budget/DayModel.swift
@@ -0,0 +1,9 @@
+//
+//  DayModel.swift
+//  Perfect Budget
+//
+//  Created by Isaak Meier on 7/12/19.
+//  Copyright © 2019 Isaak Meier. All rights reserved.
+//
+
+import Foundation
--- Perfect Budget/ProgessBarView.swift
+++ Perfect Budget/ProgessBarView.swift
@@ -13,8 +13,11 @@ import Anchorage
 class ProgessBarView: UIView {
 
     private var progessBar = UIProgressView()
+    private var currSpendLabel = UILabel()
+    private var maxSpendLabel = UILabel()
     var maxSpending = 40
     var currentSpending = 10
+    var currentHeight: CGFloat = 0
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -23,6 +26,15 @@ class ProgessBarView: UIView {
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let newHeight = self.bounds.height
+        if newHeight != currentHeight {
+            self.currentHeight = newHeight
+            configureTrack(height: newHeight)
+        }
     }
 }
 
@@ -34,7 +46,7 @@ private extension ProgessBarView {
 
         // Style -- handled (partially) by the override of draw function
         progessBar.setProgress( Float(currentSpending) / Float(maxSpending), animated: true)
-        configureTrack()
+        configureTrack(height: currentHeight)
 
         // Layout
         // Sets them to be the same size, the size of this is handled in NameAndProgessView
@@ -43,11 +55,13 @@ private extension ProgessBarView {
 
     }
 
-    func configureTrack() {
+    func configureTrack(height: CGFloat) {
+        guard height != 0 else { return }
+
         progessBar.layer.cornerRadius = 16.0
         progessBar.clipsToBounds = true
 
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 140, height: 140))
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 140, height: height))
         let img: UIImage = renderer.image { (context) in
             // this is the image border
             UIColor(displayP3Red: 133/255, green: 187/255, blue: 101/255, alpha: 1).setStroke()
@@ -56,13 +70,13 @@ private extension ProgessBarView {
             // The color of the image itself
             UIColor(displayP3Red: 133/255, green: 187/255, blue: 101/255, alpha: 1).setFill()
             // fill her up
-            context.fill(CGRect(x: 1, y: 1, width: 140, height: 140))
+            context.fill(CGRect(x: 1, y: 1, width: 140, height: height))
 
             UIColor.black.setStroke()
             UIColor.black.setFill()
-            context.cgContext.fillEllipse(in: CGRect(x: 110, y: 55, width: 30, height: 30))
+            context.cgContext.fillEllipse(in: CGRect(x: 107, y: 6, width: 25, height: 25))
         }
-        let nineSlice = img.roundedImage.resizableImage(withCapInsets: UIEdgeInsets.init(top: 15, left: 15, bottom: 15, right: 15))
+        let nineSlice = img.roundedImage.resizableImage(withCapInsets: UIEdgeInsets.init(top: 0, left: 25, bottom: 0, right: 35))
         progessBar.progressImage = nineSlice
     }
 }
